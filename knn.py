2f9f1776fa5c8838e63fd0f8b2ac1485da9c9e41
Rosa <rosaalani99@gmail.com>
2021-09-13 17:52:06 +0200

    K Nearest Neighbors

--- knn.py
+++ knn.py
@@ -95,22 +95,15 @@ print(accuracy)
 
 
 fig, ax = plt.subplots()
-
-p1 = ax.scatter(data_train[:, 0], data_train[:, 1], c=category_train, s=25)
-p1_ = ax.scatter(data_test[:, 0], data_test[:, 1], c=category_test, s=25)
+ax.scatter(data[:, 0], data[:, 1], c=category, s=25)
 plt.title('Data split into three categories')
-ax.legend([p1, p1_], ['Categories'], scatterpoints=3)
 plt.show()
 
-
 fig2, ax = plt.subplots()
-p2 = ax.scatter(data_train[:, 0], data_train[:, 1], c=category_train, s=25)
-p2_ = ax.scatter(data_test[:, 0], data_test[:, 1], c='red', s=25)
+ax.scatter(data_train[:, 0], data_train[:, 1], c=category_train, s=25)
+p = ax.scatter(data_test[:, 0], data_test[:, 1], c='red', s=25)
 plt.title('Data split into training and test samples')
-ax.legend([p2, p2_], ['Training Data', 'Test Data'], scatterpoints=3)
-#ax.legend([p2_], ['Test Data'], scatterpoints=1)
-#legend_1 = ax.legend([p2], ['Training Data'], scatterpoints=3, loc='lower left')
-#ax.add_artist(legend_1)
+ax.legend([p], ['Test Data'])
 plt.show()
 
 #TODO
